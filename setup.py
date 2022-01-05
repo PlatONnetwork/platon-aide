89d3da7149fa96dd268b889ce85e9802d1526627
Shinnng <shinnng@outlook.com>
2022-01-05 12:05:26 +0800

    update to 1.1.0

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ deps = {
         'platon_typing>=2.2.3',
         'platon_utils>=1.10.0',
         'rlp>=1.2.0',
+        'gql>=3.0.0',
     ]
 }
 
@@ -22,7 +23,7 @@ with open('./README.md', encoding='utf-8') as readme:
 setup(
     name='platon-aide',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.0.1',
+    version='1.1.0',
     description="""An aide that helps you quickly access the platon chain and use its basic functions.""",
     long_description=long_description,
     long_description_content_type='text/markdown',
