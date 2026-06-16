ee9455579796b75c6ffe1b5ff8e8478cf9c04e1d
xuwen <xuwen@juzix.net>
2026-06-16 17:30:55 +0800

    Update deps

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import (
 
 deps = {
     'platon-aide': [
-        'web3>=5.31.3',
-        'eth_account>=0.8.0',
-        'eth_hash>=0.5.1',
-        'eth_keys>=0.4.0',
-        'eth_typing>=3.2.0',
-        'eth_utils>=2.1.0',
+        'web3>=5.23.0',
+        'eth_account>=0.5.9',
+        'eth_hash>=0.3.2',
+        'eth_keys>=0.3.4',
+        'eth_typing>=2.3.0',
+        'eth_utils>=1.10.0',
         'rlp>=1.2.0',
         'gql>=3.0.0rc0',
     ]
@@ -21,13 +21,13 @@ with open('./README.md', encoding='utf-8') as readme:
     long_description = readme.read()
 
 setup(
-    name='platon-aide',
+    name='platon_aide',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='2.0.0',
+    version='2.0.1',
     description="""An aide that helps you quickly access the platon chain and use its basic functions.""",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Steven',
+    author='ssdut.steven',
     author_email='ssdut.steven@gmail.com',
     url='https://github.com/PlatONnetwork/platon-aide',
     include_package_data=True,
